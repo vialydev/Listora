b340c545a165ad76f200f843abf73737ee1132bd
Asadbek Nishonov <v1aly.personal@gmail.com>
2025-07-18 18:08:12 +0500

    I have tested api

--- backend/ads/admin.py
+++ backend/ads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ad, AdImage, Category
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Ad)
+admin.site.register(AdImage)
